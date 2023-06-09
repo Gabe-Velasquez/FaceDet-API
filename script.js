33549e96fc0ff22688f25c82df5028c2ddc2759c
Gabe <gabriel.velasquez0116@gmail.com>
2023-06-08 20:21:04 -0600

    changed db

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {rejectUnauthorized: false },
+      ssl: { rejectUnauthorized: false },
       host: process.env.DATABASE_HOST,
       port: 5432,
       user: process.env.DATABASE_USER,
@@ -20,6 +20,7 @@ const db = knex({
       database: process.env.DATABASE_DB
     }
   });
+  
 //   db.select('*').from('users').then(data => {
 //   });
 // const db = require('knex')({
